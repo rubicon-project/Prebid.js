46cd793ac7b3eb81239dfdb7f2c1671fce506480
rmartinez <Rachael24!>
2020-02-19 13:06:13 -0800

    asdf

--- test/test_index.js
+++ test/test_index.js
@@ -6,34 +6,4 @@ var testsContext = require.context('.', true, /(priceFloors)|(pbjs_api)_spec$/);
 
 testsContext.keys().forEach(testsContext);
 
-// [
-//   // './spec/AnalyticsAdapter_spec',
-//   // './spec/adUnits_spec',
-//   // './spec/adapters/adbutler_spec',
-//   // './spec/adloader_spec',
-//   // './spec/aliasBidder_spec',
-//   // './spec/api_spec',
-//   // './spec/auctionmanager_spec',
-//   // './spec/config_spec',
-//   // './spec/cpmBucketManager_spec',
-//   // './spec/debugging_spec',
-//   './spec/modules/priceFloors_spec',
-//   // './spec/native_spec',
-//   // './spec/refererDetection_spec',
-//   // './spec/renderer_spec',
-//   // './spec/sizeMapping_spec',
-//   // './spec/unit/adServerManager_spec',
-//   // './spec/unit/adUnits_spec',
-//   // './spec/unit/core/adapterManager_spec',
-//   // './spec/unit/core/bidderFactory_spec',
-//   // './spec/unit/core/targeting_spec',
-//   './spec/unit/pbjs_api_spec',
-//   // './spec/unit/secureCreatives_spec',
-//   // './spec/url_spec',
-//   // './spec/userSync_spec',
-//   // './spec/utils_spec',
-//   // './spec/videoCache_spec',
-//   // './spec/video_spec'
-// ].forEach(testsContext);
-
 window.$$PREBID_GLOBAL$$.processQueue();
