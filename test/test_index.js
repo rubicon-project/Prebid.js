24e8a44b88164a82a2b18c84480295bb88567f3f
rmartinez <Rachael24!>
2020-02-28 13:42:03 -0800

    revert newline

--- test/test_index.js
+++ test/test_index.js
@@ -3,7 +3,6 @@ require('test/mocks/adloaderStub.js');
 require('test/mocks/xhr.js');
 
 var testsContext = require.context('.', true, /_spec$/);
-
 testsContext.keys().forEach(testsContext);
 
 window.$$PREBID_GLOBAL$$.processQueue();
